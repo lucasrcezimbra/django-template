7b695e3ef99dc833b3c409e515feaff0d2e2518b
Lucas Rangel Cezimbra <lucas.cezimbra@gmail.com>
2022-01-11 18:13:09 -0300

    Fix intall to copy .github/ #113

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 pip install django --upgrade \
-    && django-admin startproject --template=https://github.com/lucasrcezimbra/django-template/archive/master.zip --name=Procfile,env-sample --extension=env,ini,txt,yml $PROJECT \
+    && django-admin startproject --template=https://github.com/lucasrcezimbra/django-template/archive/master.zip --name=Procfile,env-sample --extension=env,ini,txt,yml $PROJECT --exclude \
     && cd $PROJECT \
     && rm install.sh \
     && python -m venv .venv \
