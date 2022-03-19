9896d324e7aa82101b47643d26e857bc02c5e765
Lucas Rangel Cezimbra <lucas.cezimbra@gmail.com>
2022-03-18 21:24:44 -0300

    Update install.sh
    Run docker-compose first to remove sleep

--- install.sh
+++ install.sh
@@ -4,13 +4,12 @@ pip install django --upgrade \
     && django-admin startproject --template=https://github.com/lucasrcezimbra/django-template/archive/master.zip --name=Procfile,env-sample --extension=env,ini,txt,yml $PROJECT --exclude \
     && cd $PROJECT \
     && rm install.sh LICENSE README.md \
+    && docker-compose up -d \
     && python -m venv .venv \
     && source .venv/bin/activate \
     && pip install -r requirements-dev.txt \
     && git init \
     && pre-commit install \
-    && docker-compose up -d \
-    && sleep 5 \
     && python manage.py makemigrations \
     && python manage.py migrate \
     && python manage.py runserver
