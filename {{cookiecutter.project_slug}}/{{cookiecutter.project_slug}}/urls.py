d3f79f9204d34cd3e49153c9991318e8f4327fc0
Lucas Rangel Cezimbra <lucas.cezimbra@gmail.com>
2023-08-04 16:23:18 -0300

    Improve pre-commit

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/urls.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/urls.py
@@ -1,15 +1,14 @@
 from django.contrib import admin
 from django.urls import path
 
-from {{cookiecutter.project_slug}}.core.views import index
 {%- if cookiecutter.use_django_ninja == 'True' %}
 from {{cookiecutter.project_slug}}.core.api import api
-
 {%- endif %}
+from {{cookiecutter.project_slug}}.core.views import index
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", index, name='index'),
+    path("", index, name="index"),
     {%- if cookiecutter.use_django_ninja == 'True' %}
     path("api/", api.urls),
     {%- endif %}
