54de9e8722d86e7f4374cf9fad2711c577ce126d
Lucas Rangel Cezimbra <lucas.cezimbra@gmail.com>
2023-08-07 13:12:30 -0300

    Remove django.contrib.staticfiles

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/settings.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/settings.py
@@ -20,7 +20,6 @@ INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
-    "django.contrib.staticfiles",
     "django_extensions",
 ]
 
