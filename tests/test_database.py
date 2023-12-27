389ed24854f0a6ea28df12b03a6365eb610b00b6
Lucas Rangel Cezimbra <lucas.cezimbra@gmail.com>
2023-12-27 15:36:15 -0300

    Fix GitHub Action for SQLite

--- tests/test_database.py
+++ tests/test_database.py
@@ -12,6 +12,11 @@ def test_default_postgres(cookies):
     with open(result.project / ".env") as f:
         assert "DATABASE_URL=postgres://api:p4ssw0rd@localhost:5432/api" in f.read()
 
+    with open(result.project / ".github" / "workflows" / "python-app.yml") as f:
+        content = f.read()
+        assert "docker-compose up -d" in content
+        assert "sleep 5" in content
+
 
 def test_sqlite(cookies):
     result = cookies.bake(extra_context={"database": "SQLite"})
@@ -23,3 +28,8 @@ def test_sqlite(cookies):
 
     with open(result.project / ".env") as f:
         assert "DATABASE_URL=sqlite:///db.sqlite3" in f.read()
+
+    with open(result.project / ".github" / "workflows" / "python-app.yml") as f:
+        content = f.read()
+        assert "docker-compose up -d" not in content
+        assert "sleep 5" not in content
